01cfc68c183fa04817afdbca5cc9e6f9a553cd46
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-18 17:22:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,12 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  count = 0
+  while count <= attendees.length-1
+    puts badge_maker
+    count += 1
+  end
+  
