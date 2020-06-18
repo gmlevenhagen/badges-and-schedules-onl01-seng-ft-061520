8786708c7b4a68d1182d15f2f78327eaf67918df
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-18 18:03:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,7 +17,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
+  batch_badge_creator(attendees).each do |badges|
     puts badges
   end
 
